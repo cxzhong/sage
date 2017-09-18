d423f8a934e9a6f25f79f212826ccb3a9d2f6f05
Martin Rubey <martin.rubey@tuwien.ac.at>
2017-09-18 20:23:18 +0200

    honor check=False in all constructors

--- src/sage/combinat/binary_tree.py
+++ src/sage/combinat/binary_tree.py
@@ -4160,12 +4160,24 @@ class BinaryTrees_all(DisjointUnionEnumeratedSets, BinaryTrees):
         EXAMPLES::
 
             sage: B = BinaryTrees()
-            sage: B._element_constructor_([])
-            [., .]
+
             sage: B([[],[]]) # indirect doctest
             [[., .], [., .]]
+
             sage: B(None)    # indirect doctest
             .
+
+        TESTS:
+
+        Check that checking is disabled with ``check=False``::
+
+            sage: B([[], [], []]) # indirect doctest
+            Traceback (most recent call last):
+            ...
+            ValueError: this is not a binary tree
+
+            sage: B([[], [], []], check=False) # indirect doctest
+            [[., .], [., .], [., .]]
         """
         return self.element_class(self, *args, **keywords)
 
@@ -4312,9 +4324,15 @@ class BinaryTrees_size(BinaryTrees):
             sage: S = BinaryTrees(1)   # indirect doctest
             sage: S([])
             [., .]
+
+        TESTS::
+
+            sage: S = BinaryTrees(0)
+            sage: S([], check=False)   # indirect doctest
+            [., .]
         """
         res = BinaryTree(*args, **keywords)
-        if res.number_of_nodes() != self._size:
+        if keywords.get('check', True) and res.node_number() != self._size:
             raise ValueError("wrong number of nodes")
         return res
 
@@ -4379,19 +4397,27 @@ class FullBinaryTrees_all(DisjointUnionEnumeratedSets, BinaryTrees):
         EXAMPLES::
 
             sage: FB = BinaryTrees(full=True)
-            sage: FB._element_constructor_([])
-            [., .]
+
             sage: FB([[],[]]) # indirect doctest
             [[., .], [., .]]
+
             sage: FB(None)    # indirect doctest
             .
-            sage: FB([None, []]) #indirect doctest
+            sage: FB([None, []]) # indirect doctest
             Traceback (most recent call last):
             ...
             ValueError: not full
+
+        TESTS:
+
+        Check that checking is disabled with ``check=False``::
+
+            sage: FB = BinaryTrees(full=True)
+            sage: FB([None, []], check=False) # indirect doctest
+            [., [., .]]
         """
         res = BinaryTree(*args, **keywords)
-        if not res.is_full():
+        if keywords.get('check', True) and not res.is_full():
             raise ValueError("not full")
         return res
 
@@ -4582,12 +4608,24 @@ class FullBinaryTrees_size(BinaryTrees):
             Traceback (most recent call last):
             ...
             ValueError: not full
+
+        TESTS:
+
+        Check that checking is disabled with ``check=False``::
+
+            sage: FB5 = BinaryTrees(5, full=True)
+            sage: FB5([[], [None, [None, []]]], check=False)
+            [[., .], [., [., [., .]]]]
+
+            sage: FB5(check=False)
+            [., .]
         """
         res = BinaryTree(*args, **keywords)
-        if res.number_of_nodes() != self._size:
-            raise ValueError("wrong number of nodes")
-        if not res.is_full():
-            raise ValueError("not full")
+        if keywords.get('check', True):
+            if res.node_number() != self._size:
+                raise ValueError("wrong number of nodes")
+            if not res.is_full():
+                raise ValueError("not full")
         return res
 
 
