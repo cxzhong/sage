ce396e4e2164c2f7ea9ec5e3faa4e692a8f59b86
Michael Orlitzky <michael@orlitzky.com>
2026-04-25 07:30:19 -0400

    tools/update-meson.py: remove now-unused ignored_files variable
    The one entry in this list was removed earlier.

--- tools/update-meson.py
+++ tools/update-meson.py
@@ -96,7 +96,7 @@ def update_python_sources(self: Rewriter, visitor: AstPython):
         to_append: list[StringNode] = []
         for file in python_files:
             file_name = file.name
-            if file_name in src_list or file_name in ignored_files:
+            if file_name in src_list:
                 continue
             token = Token("string", target.filename, 0, 0, 0, None, file_name)
             to_append += [StringNode(token)]
