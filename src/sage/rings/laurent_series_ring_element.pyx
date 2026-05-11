70e0f2cc3be1db0a2c3354e619d2d0c67868b79f
erosm13 <erosmangattu@gmail.com>
2026-05-11 17:57:15 -0400

    Update src/sage/rings/laurent_series_ring_element.pyx
    simplify code
    
    Co-authored-by: Martin Rubey <axiomize@yahoo.de>

--- src/sage/rings/laurent_series_ring_element.pyx
+++ src/sage/rings/laurent_series_ring_element.pyx
@@ -1677,9 +1677,7 @@ cdef class LaurentSeries(AlgebraElement):
         from sage.misc.superseded import deprecation_cython
         deprecation_cython(40576, 'reverse is deprecated; use revert instead')
 
-        f = self
-        g = f.revert(precision)
-        return g
+        return self.revert(precision)
 
     compositional_inverse = revert
 
