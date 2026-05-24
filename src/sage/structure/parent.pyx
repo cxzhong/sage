c938721b56a48e0b043cd66b28a2ef9c22456bcc
Martin Rubey <martin.rubey@tuwien.ac.at>
2026-05-24 15:24:07 +0200

    remove parent_base and the old parent

--- src/sage/structure/parent.pyx
+++ src/sage/structure/parent.pyx
@@ -88,6 +88,41 @@ This came up in some subtle bug once::
 
     sage: gp(2) + gap(3)                                                                # needs sage.libs.gap sage.libs.pari
     5
+
+Tests from parent_old::
+
+    sage: V = VectorSpace(GF(2,'a'), 2)
+    sage: V.list()
+    [(0, 0), (1, 0), (0, 1), (1, 1)]
+    sage: MatrixSpace(GF(3), 1, 1).list()
+    [[0], [1], [2]]
+    sage: DirichletGroup(3).list()
+    [Dirichlet character modulo 3 of conductor 1 mapping 2 |--> 1,
+     Dirichlet character modulo 3 of conductor 3 mapping 2 |--> -1]
+
+    sage: # needs sage.rings.finite_rings
+    sage: K = GF(7^6,'a')
+    sage: K.list()[:10]
+    [0, 1, 2, 3, 4, 5, 6, a, a + 1, a + 2]
+    sage: K.<a> = GF(4)
+    sage: K.list()
+    [0, a, a + 1, 1]
+
+    sage: QQ['q,t'].coerce_map_from(int)
+    Composite map:
+      From: Set of Python objects of class 'int'
+      To:   Multivariate Polynomial Ring in q, t over Rational Field
+      Defn:   Native morphism:
+              From: Set of Python objects of class 'int'
+              To:   Rational Field
+            then
+              Polynomial base injection morphism:
+              From: Rational Field
+              To:   Multivariate Polynomial Ring in q, t over Rational Field
+
+   sage: R.<x,y> = QQ[]
+   sage: R._generic_convert_map(QQ).category_for()
+   Category of sets with partial maps
 """
 # ****************************************************************************
 #       Copyright (C) 2009 Robert Bradshaw <robertwb@math.washington.edu>
